23c33bb3d18f31a630ab88a5902847cfb01fc28f
k-kohey <kawagutsuchi@gmail.com>
2021-03-18 21:16:44 +0900

    add project

---
+++ bored-widget/bored-widget-widget/bored_widget_widget.swift
@@ -0,0 +1,69 @@
+//
+//  bored_widget_widget.swift
+//  bored-widget-widget
+//
+//  Created by Kohei Kawaguchi on 2021/03/18.
+//
+
+import WidgetKit
+import SwiftUI
+import Intents
+
+struct Provider: IntentTimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date(), configuration: ConfigurationIntent())
+    }
+
+    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date(), configuration: configuration)
+        completion(entry)
+    }
+
+    func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate, configuration: configuration)
+            entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+    let configuration: ConfigurationIntent
+}
+
+struct bored_widget_widgetEntryView : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        Text(entry.date, style: .time)
+    }
+}
+
+@main
+struct bored_widget_widget: Widget {
+    let kind: String = "bored_widget_widget"
+
+    var body: some WidgetConfiguration {
+        IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
+            bored_widget_widgetEntryView(entry: entry)
+        }
+        .configurationDisplayName("My Widget")
+        .description("This is an example widget.")
+    }
+}
+
+struct bored_widget_widget_Previews: PreviewProvider {
+    static var previews: some View {
+        bored_widget_widgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
+            .previewContext(WidgetPreviewContext(family: .systemSmall))
+    }
+}
---
+++ bored-widget/bored-widget/bored_widgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  bored_widgetApp.swift
+//  bored-widget
+//
+//  Created by Kohei Kawaguchi on 2021/03/18.
+//
+
+import SwiftUI
+
+@main
+struct bored_widgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
